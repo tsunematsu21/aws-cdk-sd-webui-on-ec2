a544fc662876c39f9e9dd62188e08f433f8142cc
tsunematsu21 <tsunematsu21@users.noreply.github.com>
2023-10-29 17:07:46 +0900

    Initial commit

---
+++ test/aws-cdk-sd-webui-on-ec2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkSdWebuiOnEc2 from '../lib/aws-cdk-sd-webui-on-ec2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-sd-webui-on-ec2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkSdWebuiOnEc2.AwsCdkSdWebuiOnEc2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
